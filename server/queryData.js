d63e8614addbc9ff7dcd608e779d0dadc7918217
xu-hao <xuh@email.unc.edu>
2018-07-11 12:15:35 -0400

    Update queryData.js

--- server/queryData.js
+++ server/queryData.js
@@ -1,3 +1,13 @@
 const elasticsearch = require('elasticsearch')
 const client = new elasticsearch.Client({ host: { host : "localhost", port : 9200 } })
 
+async function queryBookData (term) {
+  return client.search({ 
+    index : "library", type : "book", 
+    body: {
+      query: { 
+        match: { text: { query: term } } 
+      }
+    } 
+  })
+}
